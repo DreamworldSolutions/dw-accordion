27b92d188be12412328ee504bd1e308f6c74c457
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-06-15 17:14:01 +0530

    feat(dw-accordion): Develop accordion element & it's demo.

---
+++ demo/demo-accordion.js
@@ -0,0 +1,40 @@
+import { LitElement, html, css } from 'lit-element'
+import { DwAccordion } from '../dw-accordion';
+
+class DemoAccordion extends DwAccordion {
+  constructor() {
+    super();
+    this.items = [{
+      title: 'First Title',
+      content: 'First content'
+    },
+    {
+      title: 'Second Title',
+      content: 'Second content'
+      },
+      {
+        title: 'Third Title',
+        content: 'Third content'
+      },
+      {
+        title: 'Fourth Title',
+        content: 'Fourth content'
+      },
+      {
+        title: 'Fifth Title',
+        content: 'Fifth content'
+      }]
+  }
+  
+  
+  headerTemplate(item) {
+    return html`<div>${ item.title }</div>`
+  }
+
+  contentTemplate(item) {
+    return html`<h2>${ item.title }</h2>`
+  }
+
+}
+
+customElements.define('demo-accordion', DemoAccordion);
